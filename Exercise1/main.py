f3c3f0ebc6008d25215fb1e764f27e229746adb1
Somnath Sikdar <somnath.sikdar@gmail.com>
2017-07-05 14:03:43 +0200

    modified linear model choosing

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -1,5 +1,5 @@
 from data_loader import load_data
-from train_model import train_model, prediction_accuracy, plot_regression
+from train_model import train_model, prediction_accuracy, plot_model_predictions
 
 X, y = load_data()
 assert X.shape[0] == y.shape[0]
@@ -15,7 +15,7 @@ X_test = X[last_training_row:]
 y_test = y[last_training_row:]
 assert X_test.shape[0] == y_test.shape[0]
 
-model = train_model(X_train, y_train)
+model = train_model(X_train, y_train, **{'model_type': 'logistic'})
 mean, var = prediction_accuracy(model, X_test, y_test)
 print("MSE = {}; Var = {}". format(mean, var))
-plot_regression(model, X_test, y_test)
+plot_model_predictions(model, X_test, y_test)
--- Exercise1/train_model.py
+++ Exercise1/train_model.py
@@ -1,27 +1,37 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def train_model(X_train, y_train):
-    regression_model = LinearRegression()
-    regression_model.fit(X=X_train, y=y_train)
-    return regression_model
+MODEL_TYPE = {'linear': LinearRegression,
+              'logistic': LogisticRegression}
 
-def predict(regression_model, X_test):
-    return regression_model.predict(X_test)
+def train_model(X_train, y_train, **kwargs):
+    _model = LinearRegression
+    if kwargs:
+        _model = MODEL_TYPE[kwargs['model_type']]
 
-def prediction_accuracy(regression_model, X_test, y_test):
-    prediction = regression_model.predict(X_test)
+    model = _model()
+    model.fit(X=X_train, y=y_train)
+    return model
+
+
+def predict(model, X_test):
+    return model.predict(X_test)
+
+
+def prediction_accuracy(model, X_test, y_test):
+    prediction = model.predict(X_test)
     mean = np.mean((prediction - y_test) ** 2)
-    var = regression_model.score(X_test, y_test)
+    var = model.score(X_test, y_test)
     return mean, var
 
-def plot_regression(regression_model, X_test, y_test):
+
+def plot_model_predictions(model, X_test, y_test):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(xs=X_test[:, [0]], ys=X_test[:, [1]], zs=y_test, color='black')
-    ax.scatter(xs=X_test[:, [0]], ys=X_test[:, [1]], zs=regression_model.predict(X_test), color='blue')
+    ax.scatter(xs=X_test[:, [0]], ys=X_test[:, [1]], zs=model.predict(X_test), color='blue')
 
     ax.set_xlabel('X_label')
     ax.set_ylabel('Y_label')
