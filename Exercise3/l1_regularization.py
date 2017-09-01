201f72d67452e38632a0d89cc4abd29d817d8a24
Somnath Sikdar <somnath.sikdar@gmail.com>
2017-09-01 21:33:18 +0200

    moved around code

--- Exercise3/l1_regularization.py
+++ Exercise3/l1_regularization.py
@@ -13,11 +13,13 @@ def optimize_theta(X: matrix, y: matrix, L1_param: float):
     """
     n_cols = X.shape[1]
 
-    theta = np.matrix(n_cols * [1]).T
-    change_in_theta = 1.0
+    theta = np.matrix(n_cols * [0]).T
+    theta_new = np.matrix(n_cols * [1]).T
 
-    while change_in_theta > 1e-5:
-        theta_new = np.matrix.copy(theta)
+    while np.linalg.norm(theta - theta_new) > 1e-5:
+        theta_new = theta
+
+        print(theta_new)
 
         for i in range(len(theta)):
             X_i = X[:, i]
@@ -43,14 +45,14 @@ def optimize_theta(X: matrix, y: matrix, L1_param: float):
             else:
                 theta_new[i] = theta_i_neg
 
-        change_in_theta = np.linalg.norm(theta - theta_new)
         theta = theta_new
 
     return theta
 
 if __name__ == '__main__':
-    X = np.matrix([[1, 2, 3], [4, 5, 6]])
-    y = np.matrix([1, 2]).T
-    lamb = 2
+    X = np.matrix([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    true_theta = np.matrix([1.0, 2.0, 1.0]).T
+    y = X * true_theta + np.matrix([0.01, -0.1]).T
+    lamb = 10
     theta = optimize_theta(X, y, lamb)
     print(theta)
