c2c6e942bb908bdc6c97d39ce71b1ce403c3383d
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-02-14 08:09:15 +0100

    modified main

--- NN_DeepLearning/code/main.py
+++ NN_DeepLearning/code/main.py
@@ -2,7 +2,17 @@ from code.mnist_loader import load_data_wrapper
 from code.network2 import Network
 
 
-def main(size, epochs, mini_batch_size, eta, lmbda):
+def main(size,
+         epochs,
+         mini_batch_size,
+         eta,
+         lmbda,
+         monitor_evaluation_cost=False,
+         monitor_evaluation_accuracy=True,
+         monitor_training_cost=False,
+         monitor_training_accuracy=True,
+         monitor_weight_vector_length=False,
+         regularization='L1'):
     train, val, test = load_data_wrapper()
     net = Network(size)
     net.large_weight_initializer()
@@ -12,12 +22,12 @@ def main(size, epochs, mini_batch_size, eta, lmbda):
             eta=eta,
             evaluation_data=test,
             lmbda=lmbda,
-            monitor_evaluation_cost=False,
-            monitor_evaluation_accuracy=True,
-            monitor_training_cost=False,
-            monitor_training_accuracy=True,
-            monitor_weight_vector_length=False,
-            regularization='L1')
+            monitor_evaluation_cost=monitor_evaluation_cost,
+            monitor_evaluation_accuracy=monitor_evaluation_accuracy,
+            monitor_training_cost=monitor_training_cost,
+            monitor_training_accuracy=monitor_training_accuracy,
+            monitor_weight_vector_length=monitor_weight_vector_length,
+            regularization=regularization)
 
 
 if __name__ == '__main__':
@@ -25,5 +35,21 @@ if __name__ == '__main__':
     epochs = 30
     mini_batch_size = 30
     eta = 3.0
-    lmbda = 2.0
-    main(size, epochs, mini_batch_size, eta, lmbda)
+    lmbda = 1.0
+    monitor_evaluation_cost = False
+    monitor_evaluation_accuracy = True
+    monitor_training_cost = False
+    monitor_training_accuracy = True
+    monitor_weight_vector_length = False
+    regularization = 'L2'
+    main(size,
+         epochs,
+         mini_batch_size,
+         eta,
+         lmbda,
+         monitor_evaluation_cost,
+         monitor_evaluation_accuracy,
+         monitor_training_cost,
+         monitor_training_accuracy,
+         monitor_weight_vector_length,
+         regularization)
