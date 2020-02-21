fbaa486c988ae4338a6fedec46a71814faa09055
Somnath Sikdar <somnath.sikdar@gmail.com>
2020-02-21 08:37:19 +0100

    shape of sigmoids

--- NN_DeepLearning/3d_plots/sigmoid.py
+++ NN_DeepLearning/3d_plots/sigmoid.py
@@ -7,6 +7,7 @@ Created on Fri Feb  7 07:43:00 2020
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(z: np.array):
@@ -20,11 +21,24 @@ def weighted_input(w: np.array, b: np.array, x: np.array):
     return np.dot(w, x) + b
 
 
-if __name__ == '__main__':
-    w = np.array([2.0, 3.0]).reshape((2, 1))
-    b = np.array([1.0, 1.0]).reshape((2, 1))
-    x = np.array([5.0]).reshape((1, 1))
+def create_input(weight, bias, left, right, num_pts=1000):
+    return np.array([weight * x + bias for x in np.linspace(left, right, num_pts)])
 
-    w_x = weighted_input(w, b, x)
-    sig = sigmoid(w_x)
-    print(sig)
+if __name__ == '__main__':
+    # w = np.array([2.0, 3.0]).reshape((2, 1))
+    # b = np.array([1.0, 1.0]).reshape((2, 1))
+    # x = np.array([5.0]).reshape((1, 1))
+    #
+    # w_x = weighted_input(w, b, x)
+    # sig = sigmoid(w_x)
+    # print(sig)
+    left = -0.0
+    right = 1.0
+    num_pts = 1000
+    wt_bias = [(9.0, -4.0), (8.0, -4.0), (7.0, -4.0), (6.0, -4.0), (5.0, -4), (999.0, -400.0)]
+    for weight, bias in wt_bias:
+        x_vals = np.linspace(left, right, num_pts)
+        y_vals = sigmoid(np.array([weight * x + bias for x in x_vals]))
+        plt.plot(x_vals, y_vals, label=f'weight = {weight}, bias = {bias}')
+        plt.legend()
+    plt.show()
