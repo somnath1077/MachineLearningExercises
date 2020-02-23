fb433b1f703c34743a989f5b4d488200d3b5086c
Somnath Sikdar <somnath.sikdar@gmail.com>
2020-02-23 08:47:20 +0100

    first attempt approx func

--- NN_DeepLearning/3d_plots/approximating_functions.py
+++ NN_DeepLearning/3d_plots/approximating_functions.py
@@ -73,7 +73,7 @@ def f1_nielsen(x):
 
 if __name__ == '__main__':
 
-    samples = create_samples(f1_nielsen, left=-1.0, right=5.0, num_samples=1000)
+    samples = create_samples(f1_nielsen, left=-1.0, right=4.0, num_samples=2000)
     net = build_network(samples)
     net_vals = evaluate_network(samples, net)
 
