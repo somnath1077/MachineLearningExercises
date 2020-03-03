f0ced233a3cce11358c251d2f007235ad43ce739
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-03-03 08:00:17 +0100

    pep8

--- NN_DeepLearning/other_code/draw_nn.py
+++ NN_DeepLearning/other_code/draw_nn.py
@@ -8,6 +8,7 @@ Code copied from:
 
 import matplotlib.pyplot as plt
 
+
 def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
     '''
     Draw a neural network cartoon using matplotilb.
@@ -31,29 +32,30 @@ def draw_neural_net(ax, left, right, bottom, top, layer_sizes):
             List of layer sizes, including input and output dimensionality
     '''
     n_layers = len(layer_sizes)
-    v_spacing = (top - bottom)/float(max(layer_sizes))
-    h_spacing = (right - left)/float(len(layer_sizes) - 1)
-    
+    v_spacing = (top - bottom) / float(max(layer_sizes))
+    h_spacing = (right - left) / float(len(layer_sizes) - 1)
+
     # Nodes
     for n, layer_size in enumerate(layer_sizes):
-        layer_top = v_spacing * (layer_size - 1)/2. + (top + bottom)/2.
+        layer_top = v_spacing * (layer_size - 1) / 2. + (top + bottom) / 2.
         for m in range(layer_size):
-            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), 
-                                v_spacing/4.,
+            circle = plt.Circle((n * h_spacing + left, layer_top - m * v_spacing),
+                                v_spacing / 4.,
                                 color='w', ec='k', zorder=4)
             ax.add_artist(circle)
-    
+
     # Edges
     for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
-        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
-        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+        layer_top_a = v_spacing * (layer_size_a - 1) / 2. + (top + bottom) / 2.
+        layer_top_b = v_spacing * (layer_size_b - 1) / 2. + (top + bottom) / 2.
         for m in range(layer_size_a):
             for o in range(layer_size_b):
-                line = plt.Line2D([n*h_spacing + left, 
-                                   (n + 1)*h_spacing + left],
-                                  [layer_top_a - m*v_spacing, 
-                                   layer_top_b - o*v_spacing], c='k')
+                line = plt.Line2D([n * h_spacing + left,
+                                   (n + 1) * h_spacing + left],
+                                  [layer_top_a - m * v_spacing,
+                                   layer_top_b - o * v_spacing], c='k')
                 ax.add_artist(line)
+
 
 if __name__ == '__main__':
     fig = plt.figure(figsize=(12, 12))
