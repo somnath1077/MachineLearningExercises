bca0a735aec9105872cc5870e94d5e485e6dceea
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-03-05 12:58:43 +0100

    tanh conversions

--- NN_DeepLearning/code/network.py
+++ NN_DeepLearning/code/network.py
@@ -214,6 +214,7 @@ class Network(object):
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
+    @staticmethod
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
