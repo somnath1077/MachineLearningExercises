57902e88f2420bd79af76872a90665468d5f3e1d
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-03-24 08:58:59 +0100

    Two convolutional layers

--- NN_DeepLearning/code/main_conv_nets.py
+++ NN_DeepLearning/code/main_conv_nets.py
@@ -9,7 +9,10 @@ def conv_main():
     net = Network([ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                                  filter_shape=(20, 1, 5, 5),
                                  poolsize=(2, 2)),
-                   FullyConnectedLayer(n_in=20 * 12 * 12, n_out=100),
+                   ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                                 filter_shape=(40, 20, 5, 5),
+                                 poolsize=(2, 2)),
+                   FullyConnectedLayer(n_in=40 * 4 * 4, n_out=100),
                    SoftmaxLayer(n_in=100, n_out=10)],
                   mini_batch_size)
     net.SGD(train,
