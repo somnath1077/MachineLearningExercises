3e6ba73649484702b8c8c87cffebc8155401e723
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-06-03 08:35:10 +0200

    code reorganized

--- ML_Using_Python/chap02/s02_broadcasting.py
+++ ML_Using_Python/chap02/s02_broadcasting.py
@@ -8,8 +8,35 @@ import numpy as np
         2. The smaller tensor is repeated alongside these new axes to match the full shape of the larger tensor.
 """
 
-X = np.zeros(shape=(5, 3))
-y = np.array([1, 2, 3])
 
-print(X.ndim)
-print(y.ndim)
+# Naive implementation of broadcasting
+def naive_add_matrix_and_vector(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[1] == y.shape[0]
+    x = x.copy()
+    for i in range(x.shape[0]):
+        x[i] += y
+
+    return x
+
+
+def naive_vector_dot(x, y):
+    assert len(x.shape) == 1
+    assert len(y.shape) == 1
+    assert x.shape[0] == y.shape[0]
+
+    z = 0.
+    for i in range(x.shape[0]):
+        z += x[i] * y[i]
+    return z
+
+
+if __name__ == '__main__':
+    X = np.zeros(shape=(5, 3))
+    y = np.array([1, 2, 3])
+
+    print(f"X = {X}")
+    print(f"y = {y}")
+    print(f"X + y = {X + y}")
+    print(naive_add_matrix_and_vector(X, y))
