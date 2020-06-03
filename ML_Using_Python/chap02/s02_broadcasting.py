7ad71bb3264825a7dce5888fa744dd5799e44cda
Sikdar Somnath Dr <somnath.sikdar@metronom.com>
2020-06-03 09:52:09 +0200

    code reorganized

--- ML_Using_Python/chap02/s02_broadcasting.py
+++ ML_Using_Python/chap02/s02_broadcasting.py
@@ -32,11 +32,35 @@ def naive_vector_dot(x, y):
     return z
 
 
+def naive_matrix_vector_dot(x, y):
+    z = np.zeros(x.shape[0])
+    for i in range(x.shape[0]):
+        z[i] = naive_vector_dot(x[i, :], y)
+    return z
+
+
+def naive_matrix_dot(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 2
+    assert x.shape[1] == y.shape[0]
+
+    z = np.zeros((x.shape[0], y.shape[1]))
+    for i in range(x.shape[0]):
+        for j in range(y.shape[1]):
+            row_x = x[i, :]
+            column_y = y[:, j]
+            z[i, j] = naive_vector_dot(row_x, column_y)
+    return z
+
+
 if __name__ == '__main__':
-    X = np.zeros(shape=(5, 3))
+    X = np.ones(shape=(5, 3))
     y = np.array([1, 2, 3])
 
-    print(f"X = {X}")
-    print(f"y = {y}")
-    print(f"X + y = {X + y}")
-    print(naive_add_matrix_and_vector(X, y))
+    # print(f"X = {X}")
+    # print(f"y = {y}")
+    # print(f"X + y = {X + y}")
+    # print(naive_add_matrix_and_vector(X, y))
+    # print(naive_matrix_vector_dot(X, y))
+    B = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print(naive_matrix_dot(X, B))
