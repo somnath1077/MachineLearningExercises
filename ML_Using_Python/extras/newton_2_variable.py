6397045d7017b864187afe7e828aeb175a32d8ac
somnath1077 <somnath.sikdar@gmail.com>
2021-08-10 20:21:47 +0200

    PEP8

--- ML_Using_Python/extras/newton_2_variable.py
+++ ML_Using_Python/extras/newton_2_variable.py
@@ -12,21 +12,24 @@ import numpy as np
 def f(z):
     x = z[0][0]
     y = z[1][0]
-    
-    x_1 = - 13 + x - 2 * y + 5 * y**2 - y**3
-    y_1 = - 29 + x - 14 * y + y**2 + y**3
+
+    x_1 = - 13 + x - 2 * y + 5 * y ** 2 - y ** 3
+    y_1 = - 29 + x - 14 * y + y ** 2 + y ** 3
 
     return np.array([x_1, y_1]).reshape(2, 1)
 
 
 def numerator_expr_1(y):
-    return 3 * y**2 + 2 * y - 14
+    return 3 * y ** 2 + 2 * y - 14
+
 
 def numerator_expr_2(y):
-    return 3 * y**2 - 10 * y + 2
+    return 3 * y ** 2 - 10 * y + 2
+
 
 def denom_expr(y):
-    return 6 * y**2 - 8 * y - 12
+    return 6 * y ** 2 - 8 * y - 12
+
 
 def f_jacobian(z):
     y = z[1][0]
@@ -34,32 +37,30 @@ def f_jacobian(z):
     b = numerator_expr_2(y) / denom_expr(y)
     c = -1 / denom_expr(y)
     d = 1 / denom_expr(y)
-    
+
     return np.array([[a, b], [c, d]])
 
 
 def dist(v1, v2):
     return np.sqrt(np.sum(np.square(v1 - v2)))
 
+
 def newton(x0, y0, tol=1e-4):
     z_old = np.array([x0, y0]).reshape(2, 1)
     z_new = np.array([2 * x0, 2 * y0]).reshape(2, 1)
-    
+
     d = dist(z_old, z_new)
-    iter = 0
-    
+    it = 0
+
     while d > tol:
         z_new = z_old - np.matmul(f_jacobian(z_old), f(z_old))
         d = dist(z_new, z_old)
-        print(f'iteration: {iter:3d}: x: {z_old[0][0]:10.5f}, y: {z_old[1][0]:10.5f}')
+        print(f'iteration: {it:3d}: x: {z_old[0][0]:10.5f}, y: {z_old[1][0]:10.5f}')
         z_old = z_new
-        iter += 1
-    
+        it += 1
+
     return z_new
+
 
 if __name__ == '__main__':
     newton(10, 8)
-    
-    
-    
-
